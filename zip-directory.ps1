1cb10b504f1c70007925e904ce1331511600eaab
Ole Morten Didriksen <code@oledid.com>
2015-10-18 12:33:54 +0200

    Delete zip-directory.ps1

--- zip-directory.ps1
+++
@@ -1,8 +0,0 @@
-function ZipFiles($sourcedir $zipfilename)
-{
-   Add-Type -Assembly System.IO.Compression.FileSystem
-   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
-   [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir, $zipfilename, $compressionLevel, $false)
-}
-
-# ZipFiles $sourceDir $zipfilename
